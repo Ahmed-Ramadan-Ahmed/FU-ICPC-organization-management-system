271820e33f2d32b9984999d7cb1122d672341cb5
Ahmed Ramadan <78470901+Ahmed-Ramadan-Ahmed@users.noreply.github.com>
2023-04-05 16:58:31 +0200

    Update Member.h

--- Member.h
+++ Member.h
@@ -1,8 +1,8 @@
+#ifndef FU_ICPC_MANAGEMENT_ORGANIZATION_SYSTEM_MEMBER_H
+#define FU_ICPC_MANAGEMENT_ORGANIZATION_SYSTEM_MEMBER_H
+
 #include <bits/stdc++.h>
 using namespace std;
-#ifndef FU_ICPC_ORGANIZATION_MANAGEMENT_SYSTEM_MEMBER_H
-#define FU_ICPC_ORGANIZATION_MANAGEMENT_SYSTEM_MEMBER_H
-
 
 class Member {
 private:
@@ -14,19 +14,21 @@ private:
     string Faculty ;
     string Handle ;
     string ID ;
-    int Academic_Level ;
+    string Academic_Level ;
     int static ID_Counter ;
 protected:
     void Print_Member_Data();
     Member() ;
 public:
-    Member(string name, string phone, string mail, string university, string faculty, string handle, string Membership_Type , int academic_level);
+    Member(string name, string phone, string mail, string university, string faculty, string handle, string Membership_Type , string academic_level);
 
     void Set_Name(string name) ;
     void Set_Phone(string phone) ;
     void Set_Mail(string mail) ;
+    void Set_Faculty(string Faculty) ;
+    void Set_University(string University) ;
     void Set_Handle(string handle) ;
-    void Set_Academic_Level(int academic_level) ;
+    void Set_Academic_Level(string academic_level) ;
 
     string Get_Name() ;
     string Get_Phone() ;
@@ -35,7 +37,9 @@ public:
     string Get_Faculty() ;
     string Get_ID() ;
     string Get_Handle() ;
-    int Get_Academic_Level() ;
+    string Get_Academic_Level() ;
+
+
 };
 
-#endif //FU_ICPC_ORGANIZATION_MANAGEMENT_SYSTEM_MEMBER_H
+#endif //FU_ICPC_MANAGEMENT_ORGANIZATION_SYSTEM_MEMBER_H
