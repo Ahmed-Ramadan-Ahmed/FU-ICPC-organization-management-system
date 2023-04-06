7cbff848184ef162ce71ec6dcac5f6ccbb356da5
Ahmed Ramadan <78470901+Ahmed-Ramadan-Ahmed@users.noreply.github.com>
2023-04-06 07:02:13 +0200

    Update Mentor.h

--- Mentor.h
+++ Mentor.h
@@ -8,21 +8,22 @@
 class Leader ;
 using namespace std;
 
+class Leader ;
+
 class Mentor : public Member{
     vector<Student> Students ;
     int static Mentors_Counter ;
     void Fire_Student(Student student) ;
 public:
     Mentor() ;
-//    Mentor(Mentor &mentor) ;
     Mentor(string name, string phone, string mail, string university,
            string faculty, string handle, string academic_level);
     void Insert_Student(Student student) ;
-    bool Fire_Student_with_ID(string id) ;
+    void Fire_Student_with_ID(string id) ;
     void Print_Students_Data();
     void Print_Mentor_Data() ;
-    void Add_Points_to_Student(Student student, int points) ;
-    void Add_Warning_to_Student(Student student) ;
+    void Add_Points_to_Student(Student &student, int points) ;
+    void Add_Warning_to_Student(Student &student) ;
     friend class Leader ;
 };
 
