83c17dfc5910355e1eb92c2273de66c44b26728a
Ahmed Ramadan <78470901+Ahmed-Ramadan-Ahmed@users.noreply.github.com>
2023-04-06 07:01:52 +0200

    Update Mentor.cpp

--- Mentor.cpp
+++ Mentor.cpp
@@ -6,17 +6,7 @@ using namespace std;
 
 int Mentor::Mentors_Counter = 0 ;
 Mentor::Mentor() {} ;
-//
-//Mentor::Mentor(Mentor &mentor)
-//{
-//    Set_Name(mentor.Get_Name()) ;
-//    Set_Phone(mentor.Get_Phone()) ;
-//    Set_Mail(mentor.Get_Mail()) ;
-//    Set_Handle(mentor.Get_Handle()) ;
-//    Set_Academic_Level(mentor.Get_Academic_Level()) ;
-//    Set_Faculty(mentor.Get_Faculty()) ;
-//    Set_University(mentor.Get_University()) ;
-//}
+
 Mentor::Mentor(string name, string phone, string mail, string university,
                string faculty, string handle, string academic_level)
                :Member(name,phone,mail,university,faculty,handle,"M",academic_level)
@@ -27,7 +17,9 @@ Mentor::Mentor(string name, string phone, string mail, string university,
 void Mentor::Insert_Student(Student student)
 {
     Students.push_back(student) ;
-    cout<<"Student "<<student.Get_Name()<<" has been added to class "<<Get_ID()<<endl<<endl;
+    cout<<"Student "<<student.Get_Name()<<" has been added to mentor with id "<<Get_ID()<<endl;
+    student.Print_Student_Data() ;
+
 }
 void Mentor::Print_Students_Data()
 {
@@ -53,6 +45,7 @@ void Mentor::Fire_Student(Student student)
     {
         if(Students[i].Get_ID()==student.Get_ID())
         {
+            cout<<"Student "<<Students[i].Get_Name()<<" fired successfully"<<endl<<endl;
             for(int j=i+1;j< Students.size();j++)
             {
                 Students[j-1]=Students[j] ;
@@ -63,32 +56,31 @@ void Mentor::Fire_Student(Student student)
     }
 }
 
-bool Mentor::Fire_Student_with_ID(string id)
+void Mentor::Fire_Student_with_ID(string id)
 {
     for(int i=0;i< Students.size();i++)
     {
         if(Students[i].Get_ID()==id)
         {
-            //cout<<"Student "<<Students[i].Get_Name()<<" fired successfully"<<endl<<endl;
+            cout<<"Student "<<Students[i].Get_Name()<<" fired successfully"<<endl<<endl;
             for(int j=i+1;j< Students.size() ;j++)
             {
                 Students[j-1]=Students[j] ;
             }
             Students.pop_back();
-            return 1;
+            return ;
         }
     }
-    return 0 ;
-    //cout<<"Student with ID "<<id<<" not found in this class with mentor "<< Get_Name()<<endl<<endl;
 }
-void Mentor::Add_Points_to_Student(Student student , int points)
+void Mentor::Add_Points_to_Student(Student &student , int points)
 {
     cout<<"Student "<<student.Get_Name()<<" has "<<student.Get_Points()<<" + "<<points<<" = "<<student.Get_Points()+points<<endl<<endl;
     student.Add_Points(points) ;
 }
-void Mentor::Add_Warning_to_Student(Student student)
+void Mentor::Add_Warning_to_Student(Student &student)
 {
     student.Warning_Times++ ;
+    cout<<"Student "<<student.Get_Name()<<" has been warned"<<student.Get_Warning_Times()<<endl<<endl;
 };
 
 
